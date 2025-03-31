9efbd707c5925ce60591c69c0eced056bcbdad5e
pedrosiqueira <pedro.siqueira@ifms.edu.br>
2025-03-31 18:42:27 -0400

    brand new sveltekit+drizzle+lucia project

---
+++ src/lib/server/db/schema.js
@@ -0,0 +1,14 @@
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+
+export const user = sqliteTable('user', {
+	id: text('id').primaryKey(),
+	age: integer('age')
+});
+
+export const session = sqliteTable('session', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
+});
