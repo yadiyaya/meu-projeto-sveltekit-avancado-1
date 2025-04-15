3a1ae52ca1b632befbf62bab2993fff704327b7a
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2025-04-15 16:54:12 -0400

    organizando o layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Header from '$lib/components/Header.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+
+  let { children } = $props();
+</script>
+
+<div class="d-flex flex-column min-vh-100 container-fluid">
+  <Header />
+
+  <main>{@render children()}</main>
+
+  <Footer />
+</div>
